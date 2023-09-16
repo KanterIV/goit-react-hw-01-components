3171141986fbdfa893152e87573981d615fcf7d9
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-09-16 15:48:11 +0200

    Added styles to components

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,8 +1,9 @@
 import css from './StatisticsItem.module.css';
+import getRandomHexColor from 'helper/random-hex-color';
 
 export const StatisticsItem = ({ label, percentage }) => {
   return (
-    <li className={css.item}>
+    <li className={css.item} style={{ backgroundColor: getRandomHexColor() }}>
       <span className={css.label}>{label}</span>
       <span className={css.percentage}>{`${percentage}%`}</span>
     </li>
